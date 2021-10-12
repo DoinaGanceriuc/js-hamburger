f598561a409e792561630b1b5782722f86d84e12
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-12 15:30:10 +0200

    add html, css and js basic structure

---
+++ main.js
@@ -0,0 +1,9 @@
+// mostrare / nascondere il menu principale
+
+/* Strumenti
+
+- document.getElementById("myBtn").addEventListener("click", function() {
+  document.getElementById("demo").innerHTML = "Hello World";
+});
+
+*/
